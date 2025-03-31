918b21a1454462a2940e056be6521535b836641a
Vignesh <vrajesh0@lab.cpcc.edu>
2025-03-31 14:38:15 -0400

    Revert "Program Modified"
    This reverts commit ae33021b0dd90566cffd3cb2d290c30076ad0960.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,4 @@
 public class HelloWorld
 {
-   public static void main(String args[]){
    System.out.print("Hello World!");
-   System.out.print("Program modified, but able to revert to original version using GitHub Desktop.");
-   }
 }
